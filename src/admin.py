5b521e53776924b30eecf35c8351f05c6effca0c
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2024-01-11 16:49:51 +0000

    Halfway there

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User, Starships, Planets, Characters, FavouriteStarships, FavouritePlanets, FavouriteCharacters
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -8,9 +8,14 @@ def setup_admin(app):
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Starships, db.session))
+    admin.add_view(ModelView(Planets, db.session))
+    admin.add_view(ModelView(Characters, db.session))
+    admin.add_view(ModelView(FavouriteStarships, db.session))
+    admin.add_view(ModelView(FavouritePlanets, db.session))
+    admin.add_view(ModelView(FavouriteCharacters, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/app.py
+++ src/app.py
@@ -4,12 +4,13 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
-from flask_swagger import swagger
+from flask_swagger import Swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Starships, Planets, Characters, FavouriteStarships, FavouritePlanets, FavouriteCharacters
+
+#from models import User
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -27,11 +28,13 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
@@ -44,6 +47,133 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+db_url = os.getenv("DATABASE_URL")
+if db_url is not None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("posgres://", "posgresql://")
+
+else: 
+    app.config['SQLALCHEMY_DATABASE_URL'] = "sqlite:////tmp/test.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+MIGRATE = Migrate(app, db)
+db.init_app(app)
+CORS(app)
+setup_admin(app)
+
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+ 
+@app.route('/user', methods= ['GET'])
+def handle_hello():
+
+    response_body = {
+        "msg": "Hi this is your GET /user response"
+    }
+
+    return jsonify(response_body), 200
+
+@app.route("/starships", methods= ['GET'])
+def get_all_starships():
+ starship = Starships.query.all()
+ starship_serialized = list(map(lambda x: x.serialize(), starship))
+ return jsonify({"msg:" "Completed", "Starship": starship_serialized})
+
+@app.route('/starships/<int: starship_id>', methods=['GET'])
+def handle_starship(starship_id):
+    single_starship = Starships.query.get(starship_id)
+    if single_starship is None:
+        raise APIException(f"Starship ID not found {starship_id}", status_code=400)
+
+    response_body = {
+        "msg": "Hi this is your GET /starship response",
+        "starship_id": starship_id,
+        "starship_info": single_starship.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+@app.route("/planets", methods= ['GET'])
+def get_all_planets():
+    planets = Planets.query.all()
+    planets_serialized = list(map(lambda x: x.serialize(), planets))
+    return jsonify({"msg": "Completed", "planets": planets_serialized})
+
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def handle_planets(planets_id):
+    single_planets = Planets.query.get(planets_id)
+    if single_planets is None:
+       raise APIException(f"Planet ID not found {planets_id}", status_code=400)
+    
+    response_body = {
+        "msg": "Hello, this is your GET /planets response ",
+        "planets_id": planets_id,
+        "planets_info": single_planets.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+@app.route("/characters", methods= ['GET'])
+def get_all_characters():
+    characters = Characters.query.all()
+    characters_serialized = list(map(lambda x: x.serialize(), characters))
+    return jsonify({"msg": "Completed", "Characters": characters_serialized})
+
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def handle_characters(character_id):
+    single_character = Characters.query.get(character_id)
+    if single_character is None:
+       raise APIException(f"Character ID not found {character_id}", status_code=400)
+    
+    response_body = {
+        "msg": "Hello, this is your GET /planets response ",
+        "planets_id": character_id,
+        "planets_info": single_character.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route("/users", methods= ['GET'])
+def get_all_users():
+    users = User.query.all()
+    users_serialized = list(map(lambda x: x.serialize(), users))
+    return jsonify({"msg": "Completed", "users": users_serialized})
+
+
+@app.route('/users/<int:users_id>', methods=['GET'])
+def handle_users(user_id):
+    single_user = User.query.get(user_id)
+    if single_user is None:
+       raise APIException(f"User ID not found {user_id}", status_code=400)
+    
+    response_body = {
+        "msg": "Hello, this is your GET /users response ",
+        "users_id": user_id,
+        "users_info": single_user.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route('/users/favourites/<int:user_id>', methods= ['GET'])
+def user_favourites(user_id):
+    
+    favourite_starship = FavouriteStarships.query.filter_by(user_id = user_id)  
+    Starships = [starship_serialize() for starship in favourite_starship]
+    
+    favourite_planets = FavouritePlanets.query.filter_by(user_id = user_id)  
+    Planets = [planet_serialize() for planet in favourite_planets]
+    
+    favourite_characters = FavouriteCharacters.query.filter_by(user_id = user_id)  
+    Characters = [character_serialize() for character in favourite_characters]
+
+    return jsonify("Favourite", Starships, Planets, Characters ), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
